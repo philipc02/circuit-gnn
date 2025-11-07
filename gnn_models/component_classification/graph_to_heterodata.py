Fix: changed model to actually do circuit completion by masking different comp nodes in graphs and predicting using feature embeddings of graph around it (implemented mean pooling & now we have just 8 classes/logits as model is only predicting on component nodes); current model accuracy at around 45 percent

gnn_models/component_classification/graph_to_heterodata.py
@@ -3,69 +3,103 @@ from torch_geometric.data import HeteroData
 import networkx as nx
 import pickle
 import os
+import random
 
-def graph_to_heterodata(G: nx.Graph) -> HeteroData:
-    data = HeteroData()
+COMPONENT_TYPES = ["R", "C", "L", "V", "M", "Q", "D", "I"]
 
-    # separate nodes by type
-    node_types = ["component", "pin", "net", "subcircuit"]
-    node_id_map = {t: {} for t in node_types}
+def mask_graph(G: nx.Graph, num_masks_per_graph=4):
+    masked_graphs = []
+    component_nodes = [n for n, attr in G.nodes(data=True) if attr.get("type") == "component"]
+    components_to_mask = min(len(component_nodes), num_masks_per_graph)   # in case graph has less componentd
 
-    for idx, (n, attr) in enumerate(G.nodes(data=True)):
-        ntype = attr["type"]
-        # define unique index for node using length of list of nodes of this type and add to map indexed with node
-        node_id_map[ntype][n] = len(node_id_map[ntype])
+    if components_to_mask != num_masks_per_graph:
+        masked_components = component_nodes  # all nodes will be masked once
+    else:
+        masked_components = random.sample(component_nodes, num_masks_per_graph)  # randomly choose 4 components to hide (connected pin will be handled afterwards)
 
-        # extract feature indices
-        feat_dict = attr.get("features", {})
-        feat_tensor = torch.tensor([
-            feat_dict.get("node_type_idx", -1),
-            feat_dict.get("comp_type_idx", -1),
-            feat_dict.get("pin_type_idx", -1)
-        ], dtype=torch.long).unsqueeze(0) # unsqueeze(0) inserts a new dimension at index 0: (3,) -> (1, 3) ("one node with three features")
+    for component in masked_components:
+        G_masked = G.copy()  # work on copy of original graph
+        pin_nodes = []
+        # corresponding pin nodes
+        for neighbor in G.neighbors(component):
+            if G.nodes[neighbor].get("type") == "pin" and G.nodes[neighbor].get("component") == component:
+                pin_nodes.append(neighbor)
 
-        if "x" not in data[ntype]:
-            data[ntype].x = feat_tensor
-        else:
-            data[ntype].x = torch.cat([data[ntype].x, feat_tensor], dim=0)  # concatenate with existing features of nodes of this type
-    
-        # store label only for component nodes => prediction target!
-        if ntype == "component":
-            label = torch.tensor([feat_dict.get("comp_type_idx")], dtype=torch.long)
+        nodes_to_remove = [component] + pin_nodes  # list of all node to be removed
+        G_masked.remove_nodes_from(nodes_to_remove)  # edges connected to these nodes are removed too
+        # add as attributes component, matching pin nodes and comp type of masked node for labeling
+        G_masked.graph['masked_component'] = component
+        G_masked.graph['masked_component_type'] = G.nodes[component].get("comp_type")
+        G_masked.graph['masked_pins'] = pin_nodes
 
-            if "y" not in data["component"]:
-                data["component"].y = label
+        # collect all masked graph versions in this list
+        masked_graphs.append(G_masked)
+    return masked_graphs
+
+
+def graph_to_heterodata(G: nx.Graph, num_masks=4) -> HeteroData:
+    masked_graphs = mask_graph(G, num_masks_per_graph=num_masks)
+    hetero_data_list = []
+
+    for graph in masked_graphs:
+        data = HeteroData()
+
+        # separate nodes by type
+        node_types = ["component", "pin", "net", "subcircuit"]
+        node_id_map = {t: {} for t in node_types}
+
+        for idx, (n, attr) in enumerate(graph.nodes(data=True)):
+            ntype = attr["type"]
+            # define unique index for node using length of list of nodes of this type and add to map indexed with node
+            node_id_map[ntype][n] = len(node_id_map[ntype])
+
+            # extract feature indices
+            feat_dict = attr.get("features", {})
+            feat_tensor = torch.tensor([
+                feat_dict.get("node_type_idx", -1),
+                feat_dict.get("comp_type_idx", -1),
+                feat_dict.get("pin_type_idx", -1)
+            ], dtype=torch.long).unsqueeze(0) # unsqueeze(0) inserts a new dimension at index 0: (3,) -> (1, 3) ("one node with three features")
+
+            if "x" not in data[ntype]:
+                data[ntype].x = feat_tensor
             else:
-                data["component"].y = torch.cat([data["component"].y, label], dim=0) # concatenate with existing features of component nodes
+                data[ntype].x = torch.cat([data[ntype].x, feat_tensor], dim=0)  # concatenate with existing features of nodes of this type
 
-    # add edges
-    for u, v, eattr in G.edges(data=True):
-        # to create tuple containing information on edge
-        etype = eattr["kind"]
-        src_type = G.nodes[u]["type"]
-        dst_type = G.nodes[v]["type"]
-        edge_type = (src_type, etype, dst_type)
-        rev_edge_type = (dst_type, etype, src_type)
+        # add edges
+        for u, v, eattr in graph.edges(data=True):
+            # to create tuple containing information on edge
+            etype = eattr["kind"]
+            src_type = G.nodes[u]["type"]
+            dst_type = G.nodes[v]["type"]
+            edge_type = (src_type, etype, dst_type)
+            rev_edge_type = (dst_type, etype, src_type)
 
-        # retrieve unique node index from map and create tensor
-        src = node_id_map[src_type][u]
-        dst = node_id_map[dst_type][v]
-        edge_index = torch.tensor([[src], [dst]], dtype=torch.long)
-        rev_edge_index = torch.tensor([[dst], [src]], dtype=torch.long)
+            # retrieve unique node index from map and create tensor
+            src = node_id_map[src_type][u]
+            dst = node_id_map[dst_type][v]
+            edge_index = torch.tensor([[src], [dst]], dtype=torch.long)
+            rev_edge_index = torch.tensor([[dst], [src]], dtype=torch.long)
 
-        # dimension will be (2, number of source/destination nodes) with 2 indicating the two lists storing source and destination nodes
-        if "edge_index" not in data[edge_type]:
-            data[edge_type].edge_index = edge_index
-        else:
-            data[edge_type].edge_index = torch.cat([data[edge_type].edge_index, edge_index], dim=1) # concatenate with existing features of edges of this type (same source and destination type as well as same edge type (component to pin or pin to net))
+            # dimension will be (2, number of source/destination nodes) with 2 indicating the two lists storing source and destination nodes
+            if "edge_index" not in data[edge_type]:
+                data[edge_type].edge_index = edge_index
+            else:
+                data[edge_type].edge_index = torch.cat([data[edge_type].edge_index, edge_index], dim=1) # concatenate with existing features of edges of this type (same source and destination type as well as same edge type (component to pin or pin to net))
 
-        # add reversed relation as well (our graphs are undirected)
-        if "edge_index" not in data[rev_edge_type]:
-            data[rev_edge_type].edge_index = rev_edge_index
-        else:
-            data[rev_edge_type].edge_index = torch.cat([data[rev_edge_type].edge_index, rev_edge_index], dim=1)
+            # add reversed relation as well (our graphs are undirected)
+            if "edge_index" not in data[rev_edge_type]:
+                data[rev_edge_type].edge_index = rev_edge_index
+            else:
+                data[rev_edge_type].edge_index = torch.cat([data[rev_edge_type].edge_index, rev_edge_index], dim=1)
 
-    return data
+        # this is what model has to guess based on 
+        comp_label = graph.graph['masked_component_type']
+        data.target_comp_type = torch.tensor(COMPONENT_TYPES.index(comp_label), dtype=torch.long)
+        data.masked_comp = graph.graph['masked_component']
+        hetero_data_list.append(data)
+
+    return hetero_data_list
 
 
 def load_hetero_graphs(folder_path):

gnn_models/component_classification/model_heteroconv.py
@@ -14,10 +14,11 @@ import pandas as pd
 
 def create_heterodata_obj():
     input_folder = "../../data/data_conventional"
-    output_folder = "../../data/data_conventional_heterodata"
+    output_folder = "../../data/data_conventional_heterodata"  # this will store MASKED graphs! 
 
     splits = ["train", "val", "test"]
 
+    # the splits stay unique because we are not shuffling around
     for split in splits:
         in_dir = os.path.join(input_folder, split)
         out_dir = os.path.join(output_folder, split)
@@ -28,9 +29,10 @@ def create_heterodata_obj():
                 with open(os.path.join(in_dir, fname), "rb") as f:
                     G = pickle.load(f)
 
-                data = graph_to_heterodata(G)
-                # save heterodata object into output_folder as .pt object
-                torch.save(data, os.path.join(out_dir, fname.replace(".gpickle", ".pt")))
+                data_list = graph_to_heterodata(G)  # the function returns a list of heterodata objects!
+                for i, data in enumerate(data_list):
+                    # save heterodata objects into output_folder as .pt object, name with index
+                    torch.save(data, os.path.join(out_dir, fname.replace(".gpickle", f"_masked_{i}.pt")))
 
     print("HeteroData objects were created for all graphs.\n")
 
@@ -180,9 +182,13 @@ class HeteroSAGE(torch.nn.Module):
             if i < len(self.convs) - 1:  # no dropout after last layer
                 x_dict = {k: self.dropout(x) for k, x in x_dict.items()}
 
-        # return logits for component nodes only
-        # classifying into 9 component types (subcircuit excluded), the model needs to output 9 logits (raw, unnormalized output values from the final layer of a model, just before applying f.ex. softmax) per node
-        return self.classifier(x_dict["component"])
+        # global pooling -> mean pooling to get one prediction per graph
+        component_embeddings = x_dict["component"]  # shape:[num_component_nodes, hidden_channels]
+        graph_embedding = component_embeddings.mean(dim=0, keepdim=True)  # shape:[1, hidden_channels]
+
+        # return one prediciton per graph
+        # classifying into 8 component types (subcircuit excluded), the model needs to output 8 logits (raw, unnormalized output values from the final layer of a model, just before applying f.ex. softmax) per node
+        return self.classifier(graph_embedding) # shape:[batch_size, num_classes]
     
 def train_model(num_epochs=50, hidden_channels=64, lr=0.001, batch_size=1, num_layers=3):
 
@@ -195,7 +201,7 @@ def train_model(num_epochs=50, hidden_channels=64, lr=0.001, batch_size=1, num_l
         'learning_rate': lr,
         'batch_size': batch_size,
         'num_layers': num_layers,
-        'num_classes': 9,
+        'num_classes': 8,
         'weight_decay': 1e-5
     }
     tracker.log_params(params)
@@ -206,7 +212,7 @@ def train_model(num_epochs=50, hidden_channels=64, lr=0.001, batch_size=1, num_l
     val_loader = DataLoader(val_dataset, batch_size=batch_size)
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
-    num_classes = 9 # nodes can have following component_type values: (R, C, L, V, M, Q, D, I -> corresponding index) or no component type (-1)
+    num_classes = 8 # nodes can have following component_type values: (R, C, L, V, M, Q, D, I -> corresponding index)
 
     model = HeteroSAGE(hidden_channels=hidden_channels, num_classes=num_classes, num_layers=num_layers)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-5) # Adam -> gradient descent optimizer for model's weights (uses computed gradients)
@@ -226,7 +232,8 @@ def train_model(num_epochs=50, hidden_channels=64, lr=0.001, batch_size=1, num_l
         # compute loss for each graph in train dataset and add up
         for data in train_loader:
             out = model(data.x_dict, data.edge_index_dict) # forward pass: predict from data (using HeteroSAGE forward() function defined above)
-            loss = criterion(out, data["component"].y)
+            # CrossEntropyLoss internally applies softmax and neg log likelihood to get value with highest probability from the logits
+            loss = criterion(out, data.target_comp_type)  # our desired value is component type of missinh node
 
             optimizer.zero_grad() # resets gradients from previous iteration (otherwise gradients from multiple backward passes will add up -> like this only the current gradient is used to update the weights)
             loss.backward() # compute gradients (backward pass)
@@ -244,12 +251,12 @@ def train_model(num_epochs=50, hidden_channels=64, lr=0.001, batch_size=1, num_l
         with torch.no_grad(): # tells PyTorch not to track gradients (only needed during training when we are trying to update weights)
             for data in val_loader:
                 out = model(data.x_dict, data.edge_index_dict)
-                loss = criterion(out, data["component"].y)
+                loss = criterion(out, data.target_comp_type)
                 val_loss += loss.item()
 
                 preds = out.argmax(dim=1)   # predicted component types
                 all_preds.append(preds)
-                all_labels.append(data["component"].y)  # actual component type
+                all_labels.append(data.target_comp_type)  # actual component type
 
         avg_val_loss = val_loss / len(val_loader)
         all_preds = torch.cat(all_preds)
@@ -291,9 +298,9 @@ def train_model(num_epochs=50, hidden_channels=64, lr=0.001, batch_size=1, num_l
     with torch.no_grad():
         for data in test_loader:
             out = model(data.x_dict, data.edge_index_dict)
-            preds = out.argmax(dim=1)
+            preds = out.argmax(dim=1)  # we need to manually run argmax for the evaluation (during training our loss function takes care of this)
             all_preds.append(preds)
-            all_labels.append(data["component"].y)
+            all_labels.append(data.target_comp_type)
 
     all_preds = torch.cat(all_preds)
     all_labels = torch.cat(all_labels)